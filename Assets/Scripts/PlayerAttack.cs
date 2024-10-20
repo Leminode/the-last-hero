d31dc8b7f44fb699ec28fd60aa60bc2cd54b6e06
Simas <simas.jasin@gmail.com>
2024-10-20 23:45:52 +0300

    Added basic movement and attack animation

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField]
+    private float attackCooldown;
+
+    private Animator animator;
+    private PlayerMovement playerMovement;
+
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMovement.CanAttack())
+        {
+            Attack();
+        }
+
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        animator.SetTrigger("attack");
+        cooldownTimer = 0;
+    }
+}
